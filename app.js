e18679165ddbee1ebb6ec819ea7643b0629ada5d
Christina Thomas <Penssake@gmail.com>
2017-04-11 13:46:19 -0600

    edits

--- app.js
+++ app.js
@@ -11,10 +11,3 @@ var firstImage = new displayImages('Blog', 'blog.jpg');
 console.log(firstImage);
 var secondImage = new displayImages('Portfolio', 'portfolio.jpg');
 console.log(secondImage);
-
-// function attachToDOM(){
-//   var mainImages = document.createImage('img');
-//   displayImages.setAttribute('firstImage');
-//   displayImages.setAttribute('secondImage');
-//   document.getElementById('image-constructor').appendChild('displayImages');
-// }
