570403dfe02880a32b54a764dcfb963b1b6c4b09
Christina Thomas <Penssake@gmail.com>
2017-04-14 16:03:41 -0600

    added source data

--- scripts/data.js
+++ scripts/data.js
@@ -1,15 +1,17 @@
 'use strict';
 
 var projects = [];
+var dataSource = dataSource.this;
 
 function Project (dataSource) {
   this.title = dataSource.title;
   this.location = dataSource.location;
   this.body = dataSource.body;
-}
+  console.log(this);
 
-Project.toHtml = function() {
-
+  Project.toHtml = function() {
+    $(this).html('body');
+  };
 };
 
 Project();
