85e8bd7d7d1b55cc25fbe064fc5757b47cf9d0ad
Christina Thomas <Penssake@gmail.com>
2017-04-18 13:09:38 -0600

    add on click function to hamburger

--- scripts/data.js
+++ scripts/data.js
@@ -1,17 +1,20 @@
 'use strict';
 
-var projects = [];
-var dataSource = dataSource.this;
-
-function Project (dataSource) {
-  this.title = dataSource.title;
-  this.location = dataSource.location;
-  this.body = dataSource.body;
-  console.log(this);
-
-  Project.toHtml = function() {
-    $(this).html('body');
-  };
-};
-
-Project();
+// var projects = [];
+// var dataSource = dataSource.this;
+//
+// function Project (dataSource) {
+//   this.title = dataSource.title;
+//   this.location = dataSource.location;
+//   this.body = dataSource.body;
+//   console.log(this);
+//
+//   Project.toHtml = function() {
+//     $(this).html('body');
+//   };
+// };
+//
+// Project();
+$('#hamburger').on('click', function(){
+  $('#nav-list').show();
+});
--- scripts/dataSource.js
+++ scripts/dataSource.js
@@ -2,21 +2,23 @@
 
 var dataSource = [
   {
-    title: 'content1',
-    location: 'content1.2',
-    body: 'content1.3',
+    title: 'About',
+    body: 'content1',
   },
 
   {
-    title: 'content2',
-    location: 'content2.1',
-    body: 'content2.2',
+    title: 'Project Portfolio',
+    body: 'content2',
   },
 
   {
-    title: 'content3',
-    location: 'content3.1',
-    body: 'content3.2',
+    title: 'Blog',
+    body: 'content3',
+  },
+
+  {
+    title: 'Contact',
+    body: 'content4',
   },
 
 ];
