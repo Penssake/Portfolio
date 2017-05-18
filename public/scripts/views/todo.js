abef6c1797db39fc44dc734093c89c706f5b58c6
Christina Thomas <Penssake@gmail.com>
2017-05-18 03:55:02 -0600

    portfolio styling/added json

---
+++ public/scripts/views/todo.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function(module) {
+
+function About (todoData) {
+Object.keys(todoData).forEach(key => this [key] = todoData[key]);
+}
+
+todo.all = [];
+
+About.prototype.toHtml = function() {
+  let renderToDo = Handlebars.compile($('#todo-template').text());
+  return renderAbout(this);
+};
+
+$.getJSON('/data/todo.json', function(todoData) {
+  todoData.map(function(todoObject) {
+    let todo = new About(todoObject);
+    $('#aboutTwo').append(todo.toHtml());
+  });
+});
+
+})(window);
